2865b95ea826f87d0d7c01c455952b7bd99f440d
Maxime Daude <maximedaude.dev@gmail.com>
2021-11-16 17:26:07 +0100

    Transaction Model + Route

---
+++ platform/server/models/sequelize/Transaction.js
@@ -0,0 +1,21 @@
+const { Model, DataTypes } = require("sequelize");
+const connection = require("../../lib/db");
+class Transaction extends Model {}
+
+Transaction.init(
+    {
+        amount: DataTypes.INTEGER,
+        description: DataTypes.STRING,
+        currency: DataTypes.STRING,
+        client_first_name: DataTypes.STRING,
+        client_last_name: DataTypes.STRING,
+        client_email: DataTypes.STRING
+    },
+    {
+        sequelize: connection,
+        modelName: "transaction",
+        paranoid: true,
+    }
+);
+
+module.exports = Transaction;
