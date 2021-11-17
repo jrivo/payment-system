2567cd5ab38a5456df7c154221d0bdd469749ae1
amin <aminwastaken@gmail.com>
2021-11-17 11:32:41 +0100

    Add merchant model

--- platform/server/models/sequelize/index.js
+++ platform/server/models/sequelize/index.js
@@ -1,9 +1,11 @@
 const connection = require("../../lib/db");
 const User = require("./User");
+const Merchant = require("./Merchant");
 const Transaction = require("./Transaction");
 
 process.env.NODE_ENV === "dev" &&
   connection.sync({ alter: true }).then(() => console.log("Database synced"));
-  
+
 module.exports.User = User;
+module.exports.Merchant = Merchant;
 module.exports.Transaction = Transaction;
