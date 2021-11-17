2a0fa8b3011535b7b19cf302982cc6bd2f62a4bf
christ <christmokonda@gmail.com>
2021-11-17 17:33:25 +0100

    ADD POST confirmation & HTTP Request

--- psp/server.js
+++ psp/server.js
@@ -1,9 +1,26 @@
 require("dotenv").config();
 const express = require("express");
 const app = express() ;
-
+const url = "https://localhost:3000/transactions";
 const PORT = process.env.PORT || 3001;
+const fetch = require('node-fetch');
+
+app.post("/paiement", (res,req) => {
+        setTimeout(function () {
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }).then((res) => res.status === 200);
+            console.log('Le paiement a été effectué ')
+        }, 500)
+    }
+)
 
 app.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
 })
+
+
+
