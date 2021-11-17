2d0f58e2fddec656115c960a839b4c0d88da4b27
Maxime Daude <maximedaude.dev@gmail.com>
2021-11-17 10:45:14 +0100

    Update Request HTTP Transaction

--- platform/server/server.js
+++ platform/server/server.js
@@ -5,10 +5,12 @@ const mustacheExpress = require("mustache-express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const UserRouter = require("./routes/userSequelize");
+const TransactionRouter = require("./routes/transactionSequelize");
 
-// app.use(express.json());
+app.use(express.json());
 // app.use(express.urlencoded());
 app.use("/users", UserRouter);
+app.use("/transactions", TransactionRouter);
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
