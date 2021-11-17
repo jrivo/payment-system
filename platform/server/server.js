eb13ec2829e5447d3ca7f3ac984e7fdee5ab041d
amin <aminwastaken@gmail.com>
2021-11-17 12:24:18 +0100

    Add merchant routes

--- platform/server/server.js
+++ platform/server/server.js
@@ -6,11 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const UserRouter = require("./routes/userSequelize");
 const TransactionRouter = require("./routes/transactionSequelize");
+const MerchantRouter = require("./routes/merchantSequelize");
 
 app.use(express.json());
 // app.use(express.urlencoded());
 app.use("/users", UserRouter);
 app.use("/transactions", TransactionRouter);
+app.use("/merchants", MerchantRouter);
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
