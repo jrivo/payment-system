c907ea3ba8f9a47c8615b3af75879e0eea3c1c4b
christ <christmokonda@gmail.com>
2021-11-17 11:36:31 +0100

    UPDATE Request HTTP History

--- platform/server/models/sequelize/index.js
+++ platform/server/models/sequelize/index.js
@@ -1,9 +1,12 @@
 const connection = require("../../lib/db");
 const User = require("./User");
 const Transaction = require("./Transaction");
+const History = require("./History")
 
 process.env.NODE_ENV === "dev" &&
   connection.sync({ alter: true }).then(() => console.log("Database synced"));
-  
+
 module.exports.User = User;
 module.exports.Transaction = Transaction;
+module.exports.History = History ;
+
--- platform/server/server.js
+++ platform/server/server.js
@@ -6,11 +6,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const UserRouter = require("./routes/userSequelize");
 const TransactionRouter = require("./routes/transactionSequelize");
+const HistoryRouter = require("./routes/historySequelize");
 
 app.use(express.json());
 // app.use(express.urlencoded());
 app.use("/users", UserRouter);
 app.use("/transactions", TransactionRouter);
+app.use("/history",HistoryRouter);
 
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
