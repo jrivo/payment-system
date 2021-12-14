049cfdfcd5eca631dda54ed19d6a69af9e42a6de
Jonathan RIVO <rivo.jonathan98@gmail.com>
2021-12-14 16:14:40 +0100

    in case of conflicts

--- psp/server.js
+++ psp/server.js
@@ -1,22 +1,30 @@
 require("dotenv").config();
 const express = require("express");
-const app = express();
-const url = "https://localhost:3000/transactions";
-const PORT = process.env.PORT || 3002;
-const fetch = require("node-fetch");
+const app = express() ;
+const PORT = process.env.PORT || 3001;
+const fetch = require('node-fetch');
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-app.post("/paiement", (res, req) => {
-  setTimeout(function () {
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((res) => res.status === 200);
-    console.log("Le paiement a été effectué ");
-  }, 500);
-});
+app.use(cors())
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/paiement", (req,res) => {
+        let notificationUrl = req.body.notificationUrl
+        setTimeout(function () {
+            fetch(notificationUrl, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }).then((res) => res.status === 200);
+            console.log('Le paiement a été effectué ')
+        }, 500)
+    }
+)
 
 app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
-});
+    console.log("Server is running on port " + PORT);
+})
